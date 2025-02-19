f0b37930917bd4d6af256078f93cf5ed9aeb299d
Calvin Reed <calvin.reed@intelligentreach.com>
2025-02-19 08:23:13 +0000

    Updated config for both glazewm and wezterm. Happy with the result.

--- Config/readme.txt
+++ Config/readme.txt
@@ -1,3 +1,7 @@
 glaze & zebar -> %USERPROFILE%/.glzr
 
 wezterm -> %USERPROFILE%/.config/wezterm/wezterm.lua | %USERPROFILE%/.wezterm.lua
+
+Font: Ubuntu Mono
+
+Download from -> https://fonts.google.com/selection & Install on machine
--- Config/wezterm/wezterm.lua
+++ Config/wezterm/wezterm.lua
@@ -1,38 +1,76 @@
 local wezterm = require("wezterm")
-
-local config = {}
-
-config.animation_fps = 1
+local act = wezterm.action
+local config = wezterm.config_builder()
+local theme = 'WildCherry'
+--local theme = 'Windows 95 (base16)'
+--local theme = 'Ayu Dark (Gogh)'
+--local theme = 'Banana Blueberry'
+--local theme = 'Blue Matrix'
+------------------------- Cursor --------------------------------
 config.cursor_blink_ease_in = 'Constant'
 config.cursor_blink_ease_out = 'Constant'
 config.cursor_blink_rate = 500
-
-config.color_scheme = 'Classic Dark (base16)'
+config.default_cursor_style = "BlinkingBlock"
+------------------------- Terminal ------------------------------
+config.animation_fps = 1
 config.front_end = "OpenGL"
 config.max_fps = 144
-config.default_cursor_style = "BlinkingBlock"
-
 config.term = "xterm-256color" -- Set the terminal type
 config.enable_scroll_bar = true
-config.font = wezterm.font("JetBrains Mono Regular")
-config.cell_width = 0.9
-config.window_background_opacity = 0.9
 config.prefer_egl = true
-config.font_size = 16.0
-
-config.window_padding = {
-	left = 5,
-	right = 5,
-	top = 5,
-	bottom = 5,
-}
-
--- Tabs
-config.hide_tab_bar_if_only_one_tab = true
-config.use_fancy_tab_bar = true
-
-config.window_decorations = "TITLE | RESIZE"
+config.cell_width = 1
 config.default_prog = { "powershell.exe", "-NoLogo" }
 config.initial_cols = 80
-
+------------------------- Font & Colour Theme ------------------
+--config.font = wezterm.font("JetBrains Mono Regular")
+config.font = wezterm.font("Ubuntu Mono")
+config.font_size = 16.0
+config.color_scheme = theme
+------------------------- Window & Tabs -------------------------
+config.window_decorations = "NONE | RESIZE"
+config.window_background_opacity = 0.95
+config.window_padding = {
+  left = 5,
+  right = 5,
+  top = 5,
+  bottom = 5,
+}
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+------------------------- Key Bindings --------------------------
+config.keys = {
+  {
+  	key = "t",
+  	mods = "CTRL|SHIFT",
+  	action = act.SpawnTab 'DefaultDomain'
+  },
+  {
+   	key = "e",
+   	mods = "CTRL|SHIFT",
+   	action = act.CloseCurrentPane { confirm = true }
+  },
+  {
+	key = "h",
+	mods = "CTRL|ALT",
+	action = act.SplitPane({ 
+	  direction = "Right",
+	  size = { Percent = 50 }
+	}),
+  },
+  {
+    key = "v",
+	mods = "CTRL|ALT",
+	action = act.SplitPane({
+	  direction = "Down",
+	  size = { Percent = 50 },
+	}),
+  }
+}
+for i = 1, 8 do
+  table.insert(config.keys, {
+    key = tostring(i),
+    mods = 'CTRL|ALT',
+    action = act.ActivateTab(i - 1),
+  })
+end
 return config
